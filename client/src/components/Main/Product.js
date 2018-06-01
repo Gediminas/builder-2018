408617a0d1c77cc33a3d600106de242416f6b49d
Gediminas <gediminas.luzys@gmail.com>
2018-06-01 14:16:00 +0300

    client: logging: link to log viewer test

--- client/src/components/Main/Product.js
+++ client/src/components/Main/Product.js
@@ -5,6 +5,7 @@ export default class extends PureComponent {
   render() {
     //console.log(this.props.product.toJS())
 
+
     let product_name = this.props.product.get('product_name')
     let job_id       = this.props.product.getIn(['last_job', 'id'])
     let comment      = this.props.product.getIn(['last_job', 'data', 'comment'])
@@ -23,17 +24,21 @@ export default class extends PureComponent {
     let time_start_t   = toTime(time_start)
     let time_diff_t   = toHHMMSS(time_diff)
 
+    let log_link = `log?prod=${product_name}&time=${time_start_d}`
+
     return (
       <div className='row'>
         <div className='cell'>
           <button type="button"
-                  className="btn btn_addjob"
-                  onClick={() => this.props.add_job(product_name)}>
+            className="btn btn_addjob"
+            onClick={() => this.props.add_job(product_name)}>
             + 
           </button>
         </div>
         <div className='cell'>{product_name}</div>
-        <div className='cell status' name={job_status} >{job_status}</div>
+        <div className='cell status' name={job_status}>
+            <a href={log_link}>{job_status}</a>
+        </div>
         <div className='cell'>{at}</div>
         <div className='cell'>{cron}</div>
         <div className='cell'>{prio}</div>
