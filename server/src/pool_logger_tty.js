b8cdebaa562d00fc77a8c5b278796b4c49296577
Gediminas <gediminas.luzys@gmail.com>
2018-12-21 13:30:47 +0200

    server: Logging with time

--- server/src/pool_logger_tty.js
+++ server/src/pool_logger_tty.js
@@ -1,35 +1,50 @@
 const pool = require('./pool.js')
+const sys = require('./sys_util.js')
 require('colors')
 
 let sub = 0
 
+const log = (...args) => {
+  //args.unshift('|')
+  args.unshift(sys.get_time_string().grey)
+  console.log(...args)
+}
+
+// const script_log = (context) => {
+//   const args = Array.prototype.slice.call(arguments)
+//   args.unshift(':')
+//   args.unshift(context)
+//   args.unshift('|')
+//   args.unshift(sys.get_time_string())
+//   console.log.apply(console, args)
+// }
+
 pool.on('initialized', (param) => {
-  console.log(`initialized: "${param.time}"`.bgMagenta)
+  log(`initialized: "${param.time}"`.bgMagenta)
 })
 
 pool.on('task-starting', (param) => {
-  console.log(`Starting: "${param.task.product_id}"`.bgMagenta)
+  log(`Starting: "${param.task.product_id}"`.bgMagenta)
 })
 
 pool.on('task-added', (param) => {
-  console.log(`Added: "${param.task.product_id}"`.bgMagenta)
+  log(`Added: "${param.task.product_id}"`.bgMagenta)
 })
 
 pool.on('task-removed', (param) => {
-  console.log(`Removed: "${param.task.product_id}"`.bgMagenta)
+  log(`Removed: "${param.task.product_id}"`.bgMagenta)
 })
 
 pool.on('task-killing', (param) => {
-  console.log(`Killing: "${param.task.product_id}"`.bgMagenta)
+  log(`Killing: "${param.task.product_id}"`.bgMagenta)
 })
 
 pool.on('task-killed', (param) => {
-  console.log(`Killed: "${param.task.product_id}"`.bgMagenta)
+  log(`Killed: "${param.task.product_id}"`.bgMagenta)
 })
 
 pool.on('task-completed', (param) => {
-  console.log(`Finished: "${param.task.product_id}, ${param.task.status},
-    pid=${param.task.exec.pid}, code=${param.task.exec.exitCode}"`.bgGreen)
+  log(`Finished: "${param.task.product_id}, ${param.task.status}, pid=${param.task.exec.pid}, code=${param.task.exec.exitCode}"`.bgGreen)
 })
 
 pool.on('task-output', (param) => {
@@ -44,9 +59,9 @@ pool.on('task-output', (param) => {
   for (let i = 0; i < sub; i++) {
     spaces += '  '
   }
-  console.log(`${param.task.product_id}> `.bgMagenta, spaces, param.text.green)
+  log(`${param.task.product_id}> `.bgMagenta, spaces, param.text.green)
 })
 
 pool.on('task-output-error', (param) => {
-  console.log(`${param.task.product_id}> `.bgMagenta, param.text.red)
+  log(`${param.task.product_id}> `.bgMagenta, param.text.bkRed.yellow)
 })
--- server/src/sys_util.js
+++ server/src/sys_util.js
@@ -1,26 +1,10 @@
 const fs = require('fs');
 const moment = require('moment');
 
-exports.get_time_string = () => moment().format('YYYY-MM-DD hh:mm')
-exports.to_time_string = timestamp => moment(timestamp).format('YYYY-MM-DD hh:mm')
-exports.to_fs_time_string = timestamp => moment(timestamp).format('YYYY-MM-DD_hh-mm-ss_SSS')
+exports.get_time_string = () => moment().format('YYYY-MM-DD HH:mm:ss')
+exports.to_time_string = timestamp => moment(timestamp).format('YYYY-MM-DD HH:mm:ss')
+exports.to_fs_time_string = timestamp => moment(timestamp).format('YYYY-MM-DD_HH-mm-ss_SSS')
 
-
-exports.log = () => {
-  const args = Array.prototype.slice.call(arguments)
-  args.unshift('|')
-  args.unshift(exports.get_time_string())
-  console.log.apply(console, args)
-}
-
-exports.script_log = (context) => {
-  const args = Array.prototype.slice.call(arguments)
-  args.unshift(':')
-  args.unshift(context)
-  args.unshift('|')
-  args.unshift(exports.get_time_string())
-  console.log.apply(console, args)
-}
 
 exports.ensure_dir = (dirPath) => {
   try {
