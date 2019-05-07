b3f16cea47ecc85e555b21b2f9d86f7f3dcea0f4
Gediminas <gediminas.luzys@gmail.com>
2019-05-07 16:53:05 +0300

    server: minor

--- server/src/builder.js
+++ server/src/builder.js
@@ -16,13 +16,13 @@ require('./pool-gui.js')
 const cfgApp = require('../../_cfg/config.js')
 const cfgDef = require('../../_cfg/script_defaults.json')
 
-if (!path.isAbsolute(cfgApp.script_dir))
-  cfgApp.script_dir  = path.normalize(__dirname + '/../../' + cfgApp.script_dir)
+if (!path.isAbsolute(cfgApp.script_dir)) {
+  cfgApp.script_dir  = path.normalize(`${__dirname}/../../${cfgApp.script_dir}`)
+}
 
-if (!path.isAbsolute(cfgApp.working_dir))
-  cfgApp.working_dir = path.normalize(__dirname + '/../../' + cfgApp.working_dir)
-
-
+if (!path.isAbsolute(cfgApp.working_dir)) {
+  cfgApp.working_dir  = path.normalize(`${__dirname}/../../${cfgApp.working_dir}`)
+}
 
 const load_cfg = (script_dir, product_id) => {
   const cfgPath = path.normalize(script_dir + product_id + '/script.cfg')
