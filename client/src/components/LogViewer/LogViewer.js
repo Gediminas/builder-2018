21fd7d62de2d4c55adc84c5f31e12903ebfc0233
Gediminas <gediminas.luzys@gmail.com>
2019-06-13 21:31:45 +0300

    minor

--- client/src/components/LogViewer/LogViewer.js
+++ client/src/components/LogViewer/LogViewer.js
@@ -1,57 +1,62 @@
 import React, { PureComponent } from 'react'
 import {connect} from 'react-redux'
 import * as actionCreators from '../../action_creators'
+//import {toJS} from 'immutable'
 //import {time_to_dir} from '../../tools/date_time.js'
 
 export class LogViewer extends PureComponent {
 
-  componentDidMount() {
-    console.log('LogMount')
+  constructor() {
+    super();
+    this.state = {
+      product_id: false,
+      task_uid  : false,
+      error     : false,
+    }
+  }
+
+  componentDidUpdated() {
+    this.setState({ product_id : this.props.match.params.prod_id })
+    this.setState({ task_uid   : this.props.match.params.task_uid })
+
+    if (this.props.products) {
+      console.log(this.props.products)
+    }
+
+    // if (!this.props.products) {
+    //   this.setState({ error: 18501 })
+    //   console.log('componentDidMount END')
+    //   return;
+    // }
+
+    // if (!this.state.task_uid) {
+    //   let product = this.props.products.find(product => product.get('product_id') === this.state.product_id)
+    //   if (!product) {
+    //     this.setState({ error: 18502 })
+    //     console.log('componentDidMount END')
+    //     return;
+    //   }
+    //   this.setState({ task_uid: product.getIn(['stats', 'last_task_uid']) })
+    //   console.log('componentDidMount END')
+    // }
+
+    // if (!this.state.task_uid) {
+    //   this.setState({ error: 18503 })
+    //   console.log('componentDidMount END')
+    //   return;
+    // }
   }
 
   render() {
-    if (!this.props.products) {
-      return <div> <h2>LogViewer</h2><div>No products exist</div> </div>
-    }
 
-    let product_id = this.props.match.params.prod_id
-    let task_uid   = this.props.match.params.task_uid
-
-    /*
-    if (!task_uid) {
-      let product = this.props.products.find(product => product.get('product_id') === prod_id)
-      if (!product) {
-        return <div> <h2>LogViewer</h2><div>No logs exist for product "{prod_id}"</div> </div>
-      }
-      let time_start = product.getIn(['last_task', 'time_start'])
-      task_uid = time_to_dir(time_start)
-      return <html><meta httpEquiv='refresh' content={'0; URL=' + prod_id + '/' + task_uid} /><div>Redirecting to last log</div></html>
-    }
-    return (
-      <div>
-        <h2>LogViewer</h2>
-        LOG: {prod_id}/{task_uid}
-      </div>
-    )
-    */
-
-    let product = this.props.products.find(product => product.get('product_id') === product_id)
-    if (!product) {
-      return <div> <h3> Log: "{product_id}"</h3> <p> N/A (#18501 Product) </p> </div>
-    }
-
-    if (!task_uid) {
-      // let time_start = product.getIn(['stats', 'last_task_uid'])
-      // if (!time_start) {
-      //   return <div> <h3> Log: "{product_id}"</h3> <p> N/A (#18502 Product time) </p> </div>
-      // }
-      // task_uid = time_start ? time_to_dir(time_start) : false
-      task_uid = product.getIn(['stats', 'last_task_uid'])
-    }
+    // console.log(this.props.products)
+    // if (this.state.error) {
+    //   return <div> <h2>LogViewer</h2><div>Error: #{this.state.error}</div> </div>
+    // }
 
     return (
       <div>
-        <h3> Log: "{product_id}" ({task_uid})</h3>
+        <h3> Log: "{this.state.product_id}" ({this.state.task_uid})</h3>
 
         {(() => {
           return <div> the log </div>
@@ -62,10 +67,6 @@ export class LogViewer extends PureComponent {
 }
 
 function mapStateToProps(state) {
-
-  // let product = this.props.products.find(product => product.get('product_id') === product_id)
-  // task_uid = product.getIn(['stats', 'last_task_uid'])
-
   return {
     products: state.get('products'),
   }
