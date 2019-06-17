8dda5613bf950d53d5d1be290b3cc838d1183c2c
Gediminas <gediminas.luzys@gmail.com>
2019-06-17 15:15:30 +0300

    Client: Move log request from render() to componentDidMount() using timer as products are emitted later via socket.io emit

--- client/src/components/LogViewer/LogViewer.js
+++ client/src/components/LogViewer/LogViewer.js
@@ -10,49 +10,25 @@ export class LogViewer extends PureComponent {
     super();
     this.state = {
       product_id: false,
-      task_uid  : false,
-      error     : false,
+      task_uid:   false,
+      log: ['empty'],
     }
-    this.task_uid   = false
-    this.product_id = false
-    this.loaded = false
   }
 
   componentDidMount() {
     let task_uid   = this.props.match.params.task_uid
     let product_id = this.props.match.params.prod_id
     this.setState({ product_id, task_uid})
-  }
 
-  load() {
-    //console.log('load', this.props)
-
-    this.task_uid   = this.props.match.params.task_uid
-    this.product_id = this.props.match.params.prod_id
-
-    let task_uid = this.props.match.params.task_uid
-
-    if (!task_uid) {
-       if (!this.props.data.products) {
-         //this.setState({ error: 18501 })
-         //console.log('  no products')
-         return;
-       }
-       let product = this.props.data.products.find(product => product.get('product_id') === this.state.product_id)
-       if (!product) {
-         //this.setState({ error: 18502 })
-         return;
-       }
-      task_uid = product.getIn(['stats', 'last_task_uid'])
-    }
-
-    if (task_uid) {
-      //console.log('request task_uid ', task_uid)
-      this.props.request_log(task_uid)
-    }
-
-    this.loaded = true;
-    //console.log('  OK')
+    // load after products are initialized
+    this.initTimerId = setInterval(() => {
+      console.log('init')
+      if (this.props.data.products) {
+        clearInterval(this.initTimerId)
+        this.initTimerId = false
+        this._load()
+      }
+    }, 100);
   }
 
   componentWillUnmount() {
@@ -61,9 +37,6 @@ export class LogViewer extends PureComponent {
 
 
   render() {
-    if (!this.loaded) {
-      this.load()
-    }
 
     let task_uid = this.props.match.params.task_uid
 
@@ -79,19 +52,44 @@ export class LogViewer extends PureComponent {
     return (
       <div>
         <h3> Log: "{this.state.product_id}" ({this.state.task_uid})</h3>
-
-        {(() => {
-          return <div> the log </div>
-        })()}
-
-        <button type="button"
-                className="btn btn_addtask"
-                onClick={() => this.props.request_log(task_uid)}>
-          + 
-        </button>
+        <hr/>
+        <div>{this.state.log} </div>
+        <hr/>
+        <div> <button type="button"
+                      className="btn btn_addtask"
+                      onClick={() => this.props.request_log(task_uid)}>
+                + 
+              </button>
+        </div>
       </div>
     )
   }
+
+  _load() {
+    this.task_uid   = this.props.match.params.task_uid
+    this.product_id = this.props.match.params.prod_id
+
+    let task_uid = this.props.match.params.task_uid
+
+    if (!task_uid) {
+      if (!this.props.data.products) {
+        //this.setState({ error: 18501 })
+        //console.log('  no products')
+        return;
+      }
+      let product = this.props.data.products.find(product => product.get('product_id') === this.state.product_id)
+      if (!product) {
+        //this.setState({ error: 18502 })
+        return;
+      }
+      task_uid = product.getIn(['stats', 'last_task_uid'])
+    }
+
+    if (task_uid) {
+      this.props.request_log(task_uid)
+    }
+  }
+
 }
 
 function mapStateToProps(state) {
