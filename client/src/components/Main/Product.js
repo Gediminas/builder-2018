86893981ef3f1f689dab229b3f56f40da1ab436d
Gediminas <gediminas.luzys@gmail.com>
2019-06-26 13:35:19 +0300

    Client: Use plain functions instead of React component class

--- client/src/components/Main/Product.js
+++ client/src/components/Main/Product.js
@@ -1,63 +1,59 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import {toDate, toTime, toHHMMSS} from '../../tools/date_time.js'
 
-export default class extends PureComponent {
-  render() {
-    //console.log(this.props.product.toJS())
+function Product(props) {
+  let product_id   = props.product.get('product_id')
+  let product_name = props.product.get('product_name')
+  let task_id      = props.product.getIn(['last_task', 'id'])
+  let comment      = props.product.getIn(['last_task', 'data', 'comment'])
+  let pid          = props.product.getIn(['last_task', 'data', 'pid'])
+  let task_status  = props.product.getIn(['stats', 'status'])
+  let at           = props.product.getIn(['cfg', 'at'])
+  let cron         = props.product.getIn(['cfg', 'cron'])
+  let prio         = props.product.getIn(['cfg', 'prio'])
+  let time_diff    = props.product.getIn(['last_task', 'time_diff'])
+  let time_start   = props.product.getIn(['last_task', 'time_start'])
+  //let comment    = props.product.getIn(['cfg', 'product_comment'])
+  let debug        = props.product.getIn(['cfg', 'debug'])
 
-
-    let product_id   = this.props.product.get('product_id')
-    let product_name = this.props.product.get('product_name')
-    let task_id       = this.props.product.getIn(['last_task', 'id'])
-    let comment      = this.props.product.getIn(['last_task', 'data', 'comment'])
-    let pid          = this.props.product.getIn(['last_task', 'data', 'pid'])
-    let task_status   = this.props.product.getIn(['stats', 'status'])
-    let at           = this.props.product.getIn(['cfg', 'at'])
-    let cron         = this.props.product.getIn(['cfg', 'cron'])
-    let prio         = this.props.product.getIn(['cfg', 'prio'])
-
-    let time_diff    = this.props.product.getIn(['last_task', 'time_diff'])
-    let time_start   = this.props.product.getIn(['last_task', 'time_start'])
-    //let comment    = this.props.product.getIn(['cfg', 'product_comment'])
-    let debug        = this.props.product.getIn(['cfg', 'debug'])
-
-    let time_start_d  = ''
-    let time_start_t  = ''
-    let time_diff_t   = ''
-    if (time_start) {
-        time_start_d  = toDate(time_start)
-        time_start_t  = toTime(time_start)
-        time_diff_t   = toHHMMSS(time_diff)
-    }
-    let log_link = `log/${product_id}/${time_start_d}`
-    console.log(log_link)
-
-    return (
-      <div className='row'>
-        <div className='cell'>
-          <button type="button"
-            className="btn btn_addtask"
-            onClick={() => this.props.add_task(product_id)}>
-            + 
-          </button>
-        </div>
-        <div className='cell'>{product_name}</div>
-        <div className='cell status' name={task_status}>
-            <a href={log_link}>{task_status}</a>
-        </div>
-        <div className='cell'>{at}</div>
-        <div className='cell'>{cron}</div>
-        <div className='cell'>{prio}</div>
-        <div className='cell'>{time_diff_t}</div>
-        <div className='cell'>
-          <a className='date_small'> {time_start_d} </a>
-          &nbsp;
-          <a className='time_large'> {time_start_t} </a>
-        </div>
-        <div className='cell'>{comment}</div>
-        <div className='cell'>task={task_id}, pid={pid}, {debug}</div>
-      </div>
-    )
+  let time_start_d  = ''
+  let time_start_t  = ''
+  let time_diff_t   = ''
+  if (time_start) {
+    time_start_d  = toDate(time_start)
+    time_start_t  = toTime(time_start)
+    time_diff_t   = toHHMMSS(time_diff)
   }
+  let log_link = `log/${product_id}/${time_start_d}`
+  console.log(log_link)
+
+  return (
+    <div className='row'>
+      <div className='cell'>
+        <button type="button"
+                className="btn btn_addtask"
+                onClick={() => props.add_task(product_id)}>
+          + 
+        </button>
+      </div>
+      <div className='cell'>{product_name}</div>
+      <div className='cell status' name={task_status}>
+        <Link to={log_link}>{task_status}</Link>
+      </div>
+      <div className='cell'>{at}</div>
+      <div className='cell'>{cron}</div>
+      <div className='cell'>{prio}</div>
+      <div className='cell'>{time_diff_t}</div>
+      <div className='cell'>
+        <a className='date_small'> {time_start_d} </a>
+        &nbsp;
+        <a className='time_large'> {time_start_t} </a>
+      </div>
+      <div className='cell'>{comment}</div>
+      <div className='cell'>task={task_id}, pid={pid}, {debug}</div>
+    </div>
+  )
 }
 
+export default Product;
--- client/src/components/Main/Products.js
+++ client/src/components/Main/Products.js
@@ -1,39 +1,32 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import Product from './Product'
 
-export default class extends PureComponent {
-  getProducts() {
-    return this.props.products || []
-  }
-  render() {
-    let header = (
-      <div className='row_header'>
-        <div className='cell_header'></div>
-        <div className='cell_header'>Product</div>
-        <div className='cell_header'>Status</div>
-        <div className='cell_header'>Autotester</div>
-        <div className='cell_header'>Cron</div>
-        <div className='cell_header'>Prio</div>
-        <div className='cell_header'>Span</div>
-        <div className='cell_header'>Last</div>
-        <div className='cell_header'>Comment</div>
-        <div className='cell_header'>debug</div>
-      </div>
-    )
-    let ui_products = this.getProducts().map((product) => {
-      let product_id = product.get('product_id')
-      return <Product key={product_id} product={product} add_task={this.props.add_task} />
-    })
-    return (
-      <div className='products'>
-        <div className='table'>
-          <div className='row_header'>
-          </div>
-          {header}
-          {ui_products}
+function Products(props) {
+  let products = props.products || []
+  return (
+    <div className='products'>
+      <div className='table'>
+        <div className='row_header'>
+          <div className='cell_header'></div>
+          <div className='cell_header'>Product</div>
+          <div className='cell_header'>Status</div>
+          <div className='cell_header'>Autotester</div>
+          <div className='cell_header'>Cron</div>
+          <div className='cell_header'>Prio</div>
+          <div className='cell_header'>Span</div>
+          <div className='cell_header'>Last</div>
+          <div className='cell_header'>Comment</div>
+          <div className='cell_header'>debug</div>
         </div>
+        {
+          products.map((product) => {
+            let product_id = product.get('product_id')
+            return <Product key={product_id} product={product} add_task={props.add_task} />
+          })
+        }
       </div>
-    )
-  }
+    </div>
+  )
 }
 
+export default Products;
