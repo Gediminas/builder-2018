34d774218fc41ade25602b2f59b6d5e541e9cca5
Gediminas <gediminas.luzys@gmail.com>
2019-06-27 15:05:07 +0300

    Client: Use hooks instead of React component class (LogViewer)

--- client/src/components/LogViewer/LogViewer.js
+++ client/src/components/LogViewer/LogViewer.js
@@ -5,50 +5,46 @@ import * as actionCreators from '../../action_creators'
 //import {time_to_dir} from '../../tools/date_time.js'
 
 function LogViewer(props) {
-  const [product_id, setProductId] = useState(false)
-  const [task_uid,   setTaskUid]   = useState(false)
-  const [start_time]               = useState(false)
-  const [state_log]                = useState([])
+  const [state, setState] = useState({
+    product_id : false,
+    task_uid   : false,
+    start_time : false,
+    state_log  : [],
+  })
 
-  //console.log(product_id)
-  //console.log(task_uid)
 
   useEffect(() => {
-    console.log(product_id)
-    //console.log(task_uid)
+    setState({
+      ...state,
+      product_id: props.match.params.prod_id,
+      task_uid:   props.match.params.task_uid,
+    })
+    //console.log('EFF SET', props.match.params)
 
-    setProductId(props.match.params.prod_id)
-    setTaskUid(props.match.params.task_uid)
-
-    if (!task_uid) {
-      console.log('eval task uid')
-
+    if (!state.task_uid) {
       if (props.data.products) {
-        //load after products are initialized
-        console.log('load products')
-        const taskUid = _load(props)
-        setTaskUid(taskUid)
+        //console.log('TRY GO')
+        const taskUid = _load(props, state.product_id)
+        setState({
+          ...state,
+          task_uid: taskUid,
+        })
+        //console.log('OK', taskUid);
       }
     }
-  }, [product_id, task_uid])
-  //   if (this.state.error) {
-  //     return (
-  //       <div> 
-  //         <h3> Log: "{this.state.product_id}" ({this.state.task_uid})</h3>
-  //         <div>Error: #{this.state.error}</div>
-  //       </div>
-  //     )
-  //   }
+  }, [state.product_id, props.data.products])
+
+  //console.log('RENDER', state, props.data)
 
     return (
       <div>
-        <h3> Log: "{product_id}" [{start_time}] ({task_uid})</h3>
+        <h3> Log: "{state.product_id}" [{state.start_time}] ({state.task_uid})</h3>
         <hr/>
-        <div>{state_log} </div>
+        <div>{state.state_log} </div>
         <hr/>
         <div> <button type="button"
                       className="btn btn_addtask"
-                      onClick={() => props.request_log(product_id, task_uid, start_time)}>
+                      onClick={() => props.request_log(state.product_id, state.task_uid, state.start_time)}>
                 + 
               </button>
         </div>
@@ -56,64 +52,20 @@ function LogViewer(props) {
     )
 
 }
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     product_id: false,
-  //     task_uid:   false,
-  //     start_time: false,
-  //     log: ['empty'],
-  //   }
-  // }
 
-  // componentDidMount() {
-  //   let task_uid   = this.props.match.params.task_uid
-  //   let product_id = this.props.match.params.prod_id
-  //   this.setState({ product_id, task_uid})
-  // }
-
-  // componentDidUpdate() {
-  //   if (!this.state.task_uid) {
-  //     console.log('eval task uid')
-
-  //     if (this.props.data.products) {
-  //       // load after products are initialized
-  //       console.log('load products')
-  //       this._load()
-  //     }
-  //   }
-  // }
-
-  // render() {
-
-
-  // }
-
-function _load(props) {
-  let task_uid   = props.match.params.task_uid
-  let product_id = props.match.params.prod_id
-  let start_time = false
-
-  if (!task_uid) {
-    let product = props.data.products.find(product => product.get('product_id') === this.state.product_id)
-    if (!product) {
-      //setState({ product_id, error: 18501 })
-      return;
-    }
-    task_uid = product.getIn(['stats', 'last_task_uid'])
-    start_time = product.getIn(['stats', 'last_start_time'])
-  }
-  if (!task_uid) {
-    //setState({ product_id, error: 18502 })
+function _load(props, product_id) {
+  let product = props.data.products.find(product => product.get('product_id') === product_id)
+  if (!product) {
     return;
   }
-  //setState({ task_uid, start_time })
+  let task_uid = product.getIn(['stats', 'last_task_uid'])
+  let start_time = product.getIn(['stats', 'last_start_time'])
+  if (!task_uid) {
+    return;
+  }
   props.request_log(product_id, task_uid, start_time)
   return task_uid;
 }
-
-//}
-
 
 function mapStateToProps(state) {
   return {
