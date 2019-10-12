2a510339ddd59143f449529bf1c77fa2380c448c
Gediminas <gediminas.luzys@gmail.com>
2019-10-12 07:56:08 +0300

    server: emitHistory moved to history module

--- server/src/pool-gui.js
+++ server/src/pool-gui.js
@@ -1,7 +1,6 @@
 const socketio = require('socket.io')
 const assert = require('better-assert')
 const pool = require('./pool.js')
-const db = require('./loaders/history_loader.js')
 require('colors')
 
 const emitProducts = emitter =>
@@ -10,80 +9,80 @@ const emitProducts = emitter =>
 const emitTasks = emitter =>
   emitter.emit('state', { tasks: pool.allTasks() })
 
-const emitHistory = (emitter, show_history_limit)  =>
-  emitter.emit('state', { htasks: db.get_history(show_history_limit) })
-
 
 pool.on('initialized', (param) => {
   console.log('plugin: gui: initializing start')
-  this.show_history_limit = param.cfg.show_history_limit
   const server_port       = param.cfg.server_port
 
   console.log(`plugin: gui: Socket server starting on port: ${server_port}`.bgBlue)
 
   this.io = socketio(server_port)
   this.io.on('connection', (socket) => {
-
     console.log(`plugin: gui: Client connected: ${socket.conn.remoteAddress}`.bgBlue)
-
-    emitProducts(socket)
-    emitTasks(socket)
-    emitHistory(socket, this.show_history_limit)
-
-    socket.on('task_add', data =>
-      pool.addTask(data.product_id, { user_comment: 'user comment' }))
-
-    socket.on('task_kill', data =>
-      pool.dropTask(data.task_uid))
-
-    socket.on('request_log', data => {
-      console.log('plugin: gui: Request for logs received', data)
-
-      let task_uid = data.task_uid
-
-      if (!task_uid) {
-        let products = pool.getProducts()
-        let product = products.find(_product => _product.product_id === data.product_id)
-        task_uid = product.stats.last_task_uid
-        console.log('plugin: gui: found', task_uid)
-      }
-
-      // if (!product) {
-      //   return;
-      // }
-      // let task_uid = product.getIn(['stats', 'last_task_uid'])
-      // if (!task_uid) {
-      //   return;
-      // }
-      // props.request_log(product_id, task_uid)
-
-
-      // for (let product of products) {
-      //   if (product.id === data.product_id) {
-      //     socket.emit('state', { logs: ['here will be', 'logs'] })
-      //   }
-
-      // }
-
-      let key = data.task_uid || data.product_id
-      let logs = {}
-      logs[key] = ['Here will be logs of ', data.product_id, task_uid]
-      socket.emit('state', { logs })
-
-      console.log('>> Logs sent back: ', logs)
-    })
-
-    socket.on('sys_shutdown', (param) => {
-      // sys.log("Stoping cron tasks...")
-      // script.destroy_all()
-
-      setTimeout(() => {
-        console.log("Exit.")
-        process.exit(0)
-      }, 100)
-    });
+    pool.emit('client-connected', { socket })
   });
   console.log('plugin: gui: initializing done')
+})
+
+pool.on('client-connected', (param) => {
+  this.socket = param.socket
+  let socket = this.socket
+
+  emitProducts(socket)
+  emitTasks(socket)
+
+  socket.on('task_add', data =>
+            pool.addTask(data.product_id, { user_comment: 'user comment' }))
+
+  socket.on('task_kill', data =>
+            pool.dropTask(data.task_uid))
+
+  socket.on('request_log', data => {
+    console.log('plugin: gui: Request for logs received', data)
+
+    let task_uid = data.task_uid
+
+    if (!task_uid) {
+      let products = pool.getProducts()
+      let product = products.find(_product => _product.product_id === data.product_id)
+      task_uid = product.stats.last_task_uid
+      console.log('plugin: gui: found', task_uid)
+    }
+
+    // if (!product) {
+    //   return;
+    // }
+    // let task_uid = product.getIn(['stats', 'last_task_uid'])
+    // if (!task_uid) {
+    //   return;
+    // }
+    // props.request_log(product_id, task_uid)
+
+
+    // for (let product of products) {
+    //   if (product.id === data.product_id) {
+    //     socket.emit('state', { logs: ['here will be', 'logs'] })
+    //   }
+
+    // }
+
+    let key = data.task_uid || data.product_id
+    let logs = {}
+    logs[key] = ['Here will be logs of ', data.product_id, task_uid]
+    socket.emit('state', { logs })
+
+    console.log('>> Logs sent back: ', logs)
+  })
+
+  socket.on('sys_shutdown', (param) => {
+    // sys.log("Stoping cron tasks...")
+    // script.destroy_all()
+
+    setTimeout(() => {
+      console.log("Exit.")
+      process.exit(0)
+    }, 100)
+  });
 })
 
 pool.on('error', (param) => {
@@ -100,7 +99,6 @@ pool.on('task-started', (param) => {
 
 pool.on('task-completed', (param) => {
   emitTasks(this.io)
-  emitHistory(this.io, this.show_history_limit)
   emitProducts(this.io)
 })
 
--- server/src/pool-history.js
+++ server/src/pool-history.js
@@ -1,6 +1,9 @@
 const pool   = require('./pool.js')
 const db = require('./loaders/history_loader.js')
 
+const emitHistory = (emitter, show_history_limit)  =>
+      emitter.emit('state', { htasks: db.get_history(show_history_limit) })
+
 //const updateProducts = (db, products, product_id) => {
 // for (const product of products) {
 //   if (!product_id || product.product_id === product_id) {
@@ -11,6 +14,7 @@ const db = require('./loaders/history_loader.js')
 
 pool.on('initialized', (param) => {
   console.log('plugin: history: initializing start')
+  this.show_history_limit = param.cfg.show_history_limit
   const dbPath = `${param.cfg.working_dir}history.json`
   console.log('plugin: history: DB loading', dbPath)
 
@@ -33,6 +37,11 @@ pool.on('initialized', (param) => {
   console.log('plugin: history: initializing done')
 })
 
+pool.on('client-connected', (param) => {
+  this.socket = param.socket
+  emitHistory(this.socket, this.show_history_limit)
+})
+
 pool.on('error',            param => {})
 pool.on('task-starting',    param => {})
 pool.on('task-started',     param => {})
@@ -52,6 +61,8 @@ pool.on('task-added',       param => {
 
 pool.on('task-completed',   param => {
   db.add_history(param.task)
+
+  emitHistory(this.io, this.show_history_limit)
 
   const products = pool.getProducts()
   for (let product of products) {
