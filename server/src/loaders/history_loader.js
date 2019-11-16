55012df0e94848918b08a7c097d391c8fe1a2a16
Gediminas <gediminas.luzys@gmail.com>
2019-11-16 12:23:29 +0200

    refactoring

--- server/src/loaders/history_loader.js
+++ server/src/loaders/history_loader.js
@@ -1,65 +1,64 @@
 const Loki = require('lokijs')
 require('colors')
 
-let db_history
-let tb_history
-
 class HistoryLoader {
-  init = dbPath => new Promise((resolve) => {
-    console.log('history-loader: Loading '.blue, dbPath)
+  init(dbPath) {
+    return new Promise((resolve) => {
+      console.log('history-loader: Loading '.grey, dbPath)
 
-    db_history = new Loki(dbPath, {
-      verbose         : true,
-      autosave        : true,
-      autosaveInterval: 2000,
-      // autoload: true,//autoupdate: true //use Object.observe to update objects automatically
-    })
-
-    db_history.on('error', (e) => {
-      console.log('history-loader: ERROR: ', e)
-    })
-
-    db_history.loadDatabase({}, () => {
-      tb_history = db_history.getCollection('history')
-      if (!tb_history) {
-        console.log('history-loader: Creating '.blue, dbPath)
-        tb_history = db_history.addCollection('history', { autoupdate: true })
-
-        console.log('history-loader: Database saving...'.blue)
-        db_history.saveDatabase(() => {
-          console.log('history-loader: Database saved...'.blue)
-        })
-      }
-
-      tb_history.on('error', (e) => {
-        console.log('history-loader: ERROR: ', e)
+      this.db_history = new Loki(dbPath, {
+        verbose         : true,
+        autosave        : true,
+        autosaveInterval: 2000,
+        // autoload: true,//autoupdate: true //use Object.observe to update objects automatically
       })
 
-      resolve()
-    })
-  })
+      this.db_history.on('error', (e) => {
+        console.log('history-loader: ERROR: '.red, e)
+      })
 
-  add_history = (data) => {
-    const task = tb_history.insert(data)
+      this.db_history.loadDatabase({}, () => {
+        this.tb_history = this.db_history.getCollection('history')
+        if (!this.tb_history) {
+          console.log('history-loader: Creating '.grey, dbPath)
+          this.tb_history = this.db_history.addCollection('history', { autoupdate: true })
+
+          console.log('history-loader: Database saving...'.grey)
+          this.db_history.saveDatabase(() => {
+            console.log('history-loader: Database saved...'.grey)
+          })
+        }
+
+        this.tb_history.on('error', (e) => {
+          console.log('history-loader: ERROR: '.red, e)
+        })
+
+        resolve()
+      })
+    })
+  }
+
+  add_history(data) {
+    const task = this.tb_history.insert(data)
     task.id = task.$loki
   }
 
-  get_history = (limit) => {
+  get_history(limit) {
     if (limit) {
-      return tb_history.chain().simplesort('$loki', true).limit(limit).data()
+      return this.tb_history.chain().simplesort('$loki', true).limit(limit).data()
     }
-    return tb_history.chain().simplesort('$loki', true).data()
+    return this.tb_history.chain().simplesort('$loki', true).data()
   }
 
-  findLast_history = (query) => {
-    // return tb_history.chain().find(query).limit(1).data()
-    // const res = tb_history.chain().find(query).limit(1).data()[0]
-    // const res = tb_history.findOne(query)
-    // const res = tb_history.chain().simplesort('id', false).find(query, true).data()[0]
-    const res = tb_history.chain().find(query).simplesort('$loki', { desc: true })
-          .limit(1)
-          .data()[0]
-    // console.log(res)
+  findLast_history(query) {
+    // return this.tb_history.chain().find(query).limit(1).data()
+    // const res = this.tb_history.chain().find(query).limit(1).data()[0]
+    // const res = this.tb_history.findOne(query)
+    // const res = this.tb_history.chain().simplesort('id', false).find(query, true).data()[0]
+    const res = this.tb_history.chain()
+      .find(query).simplesort('$loki', { desc: true })
+      .limit(1)
+      .data()[0]
     return res
   }
 }
