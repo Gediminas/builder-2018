bf5ba5b843682115284cbc479ea49e7a4dceb803
Gediminas <gediminas.luzys@gmail.com>
2019-11-16 10:50:07 +0200

    minor

--- server/src/loaders/history_loader.js
+++ server/src/loaders/history_loader.js
@@ -4,60 +4,66 @@ require('colors')
 let db_history
 let tb_history
 
-exports.init = dbPath => new Promise((resolve) => {
-  console.log('history-loader: Loading '.blue, dbPath)
+class HistoryLoader {
+  init = dbPath => new Promise((resolve) => {
+    console.log('history-loader: Loading '.blue, dbPath)
 
-  db_history = new Loki(dbPath, {
-    verbose         : true,
-    autosave        : true,
-    autosaveInterval: 2000,
-    // autoload: true,//autoupdate: true //use Object.observe to update objects automatically
-  })
+    db_history = new Loki(dbPath, {
+      verbose         : true,
+      autosave        : true,
+      autosaveInterval: 2000,
+      // autoload: true,//autoupdate: true //use Object.observe to update objects automatically
+    })
 
-  db_history.on('error', (e) => {
-    console.log('history-loader: ERROR: ', e)
-  })
-
-  db_history.loadDatabase({}, (result) => {
-    tb_history = db_history.getCollection('history')
-    if (!tb_history) {
-      console.log('history-loader: Creating '.blue, dbPath)
-      tb_history = db_history.addCollection('history', { autoupdate: true })
-
-      console.log('history-loader: Database saving...'.blue)
-      db_history.saveDatabase(() => {
-        console.log('history-loader: Database saved...'.blue)
-      })
-    }
-
-    tb_history.on('error', (e) => {
+    db_history.on('error', (e) => {
       console.log('history-loader: ERROR: ', e)
     })
 
-    resolve()
+    db_history.loadDatabase({}, () => {
+      tb_history = db_history.getCollection('history')
+      if (!tb_history) {
+        console.log('history-loader: Creating '.blue, dbPath)
+        tb_history = db_history.addCollection('history', { autoupdate: true })
+
+        console.log('history-loader: Database saving...'.blue)
+        db_history.saveDatabase(() => {
+          console.log('history-loader: Database saved...'.blue)
+        })
+      }
+
+      tb_history.on('error', (e) => {
+        console.log('history-loader: ERROR: ', e)
+      })
+
+      resolve()
+    })
   })
-})
 
-exports.add_history = (data) => {
-  const task = tb_history.insert(data)
-  task.id = task.$loki
-}
-
-exports.get_history = (limit) => {
-  if (limit) {
-    return tb_history.chain().simplesort('$loki', true).limit(limit).data()
+  add_history = (data) => {
+    const task = tb_history.insert(data)
+    task.id = task.$loki
   }
-  return tb_history.chain().simplesort('$loki', true).data()
+
+  get_history = (limit) => {
+    if (limit) {
+      return tb_history.chain().simplesort('$loki', true).limit(limit).data()
+    }
+    return tb_history.chain().simplesort('$loki', true).data()
+  }
+
+  findLast_history = (query) => {
+    // return tb_history.chain().find(query).limit(1).data()
+    // const res = tb_history.chain().find(query).limit(1).data()[0]
+    // const res = tb_history.findOne(query)
+    // const res = tb_history.chain().simplesort('id', false).find(query, true).data()[0]
+    const res = tb_history.chain().find(query).simplesort('$loki', { desc: true })
+          .limit(1)
+          .data()[0]
+    // console.log(res)
+    return res
+  }
 }
 
-exports.findLast_history = (query) => {
-  // return tb_history.chain().find(query).limit(1).data()
-  // const res = tb_history.chain().find(query).limit(1).data()[0]
-  // const res = tb_history.findOne(query)
-  // const res = tb_history.chain().simplesort('id', false).find(query, true).data()[0]
-  const res = tb_history.chain().find(query).simplesort('$loki', { desc: true })
-    .limit(1)
-    .data()[0]
-  // console.log(res)
-  return res
-}
+const historyLoader = new HistoryLoader()
+historyLoader.init()
+module.exports = historyLoader
