64f40d766aa81e263fd90c49a3dfa1ada69682f9
Gediminas <gediminas.luzys@gmail.com>
2020-04-30 08:38:26 +0300

    simplify

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,7 @@
 const pool = require('./pool.js')
-const poolExecImpl = require('./pool-core-exe.js')
 require('./pool-core-sys.js')
-// require('./pool-history.js')
 require('./pool-tty.js')
-require('./pool-gui.js')
+require('./pool-socket.js')
 require('colors')
 
 const productLoader = require('./loaders/project_loader.js')
@@ -21,5 +19,5 @@ console.log('----------------------------------------------------------'.blue)
 console.log('products loading')
 productLoader(cfgApp.script_dir, (products) => {
   console.log('products loaded')
-  pool.initialize(poolExecImpl, products, cfgApp)
+  pool.initialize(products, cfgApp)
 })
--- server/src/pool.js
+++ server/src/pool.js
@@ -2,13 +2,85 @@ const events = require('events')
 const assert = require('better-assert')
 const sys = require('./sys_util')
 
+const { execFile } = require('child_process')
+const kill = require('tree-kill')
+
+const processFullLines = (origBuffer, fnDoOnFullLine) => {
+  const lines = origBuffer.split(/\r?\n/)
+  const newBuffer = lines.pop()
+  lines.forEach(fnDoOnFullLine)
+  assert(newBuffer === '' || origBuffer.slice(-1) !== '\n')
+  assert(newBuffer === '')
+  return newBuffer
+}
+
+  const startTask = (task, taskOutput) => {
+    return new Promise((resolve, reject) => {
+      const args    = [task.product.script_path]
+      const options = { cwd: task.working_dir }
+
+      const child = execFile(task.product.interpreter, args, options)
+      child.bufOut = ''
+      child.bufErr = ''
+      task.pid = child.pid
+
+      child.stdout.on('data', (data) => {
+        child.bufOut += data
+        child.bufOut = processFullLines(child.bufOut, (text) => {
+          taskOutput(task, text, 'stdout')
+        })
+      })
+
+      child.stderr.on('data', (data) => {
+        child.bufErr += data
+        child.bufErr = processFullLines(child.bufErr, (text) => {
+          taskOutput(task, text, 'stderr')
+        })
+      })
+
+      child.on('error', (error) => {
+        reject(error)
+      })
+
+      child.on('close', (exitCode) => {
+        assert(child.bufOut === '') // TODO send \n
+        assert(child.bufErr === '') // TODO send \n
+        delete task.pid
+
+        switch (exitCode) {
+        case 0:  task.result = 'OK'; break
+        case 1:  task.result = 'WARNING'; break
+        case 2:  task.result = 'ERROR'; break
+        default: task.result = 'N/A'; break
+        }
+        resolve()
+      })
+    })
+  }
+
+  const killTask = (task) => {
+    return new Promise((resolve, reject) => {
+      kill(task.pid, 'SIGTERM', (error) => { // SIGKILL
+        if (error) {
+          reject(error)
+        } else {
+          resolve()
+        }
+      })
+    })
+  }
+
+  const CanRun = (task, activeTasks) => {
+    const found_active = activeTasks.find(_task => _task.product_id === task.product_id)
+    return found_active ? true : false
+  }
+
 class Pool extends events {
-  initialize(impl, products, cfg) {
+  initialize(products, cfg) {
     this.products = products
     this.waitingTasks = []
     this.activeTasks = []
     this.maxWorkers = cfg.maxWorkers
-    this.impl = impl
     console.log('init started')
     this.emit('initialized', { cfg })
     console.log('init finished')
@@ -36,7 +108,7 @@ class Pool extends events {
     for (const task of this.activeTasks) {
       if (task.uid === taskUid) {
         this.emit('task-killing', { task })
-        this.impl.killTask(task).then(() => {
+        killTask(task).then(() => {
           this.emit('task-killed', { task })
         }).catch((error) => {
           this.emit('error', { task, error, from: 'dropTask' })
@@ -66,14 +138,14 @@ class Pool extends events {
     }
     for (const i1 in this.waitingTasks) {
       const check_task = this.waitingTasks[i1]
-      if (this.impl.CanRun(check_task, this.activeTasks)) {
+      if (CanRun(check_task, this.activeTasks)) {
         continue
       }
       const task = this.waitingTasks.splice(i1, 1)[0]
       assert(task === check_task)
       this.activeTasks.push(task)
       this.emit('task-starting', { task })
-      this.impl.startTask(task, this._taskOutput.bind(this))
+      startTask(task, this._taskOutput.bind(this))
         .then(() => {
           this._taskCompleted(task)
         })
